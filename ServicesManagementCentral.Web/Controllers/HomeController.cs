b0c46d67e8b73139c782e43a0726eb4b0ddfbf88
Pedro Castillo <agonzalez@itechdev.com.mx>
2021-05-07 01:56:28 -0500

    Se actualiza controller para que funcione con AD

--- ServicesManagementCentral.Web/Controllers/HomeController.cs
+++ ServicesManagementCentral.Web/Controllers/HomeController.cs
@@ -5,11 +5,13 @@ namespace ServicesManagement.Web.Controllers
     /// <summary>
     /// home controller
     /// </summary>
+
+    [Authorize]
     public class HomeController : BaseController
     {
-        public HomeController():base(System.Web.HttpContext.Current)
+        public HomeController() : base(System.Web.HttpContext.Current)
         {
-           
+
         }
 
         /// <summary>
@@ -32,6 +34,7 @@ namespace ServicesManagement.Web.Controllers
         //    //  return View();
         //}
 
+
         public ActionResult Index()
         {
             //if (Login == null)
@@ -41,15 +44,38 @@ namespace ServicesManagement.Web.Controllers
 
             if (Login != null)
             {
-
                 if (Login.Username.Trim().Equals("omsmercurio") && Login.Password.Trim().Equals("$oriana2021#"))
                 {
                     return RedirectToAction("Index", "Ordenes");
                 }
                 else
                 {
-                    Session["userFail"] = "Usuario o Password incorrecto";
-                    return RedirectToAction("Login", "Security");
+
+                    if (Session["loginTienda"] != null)
+                    {
+                        Session["Id_Num_UN"] = Session["loginTienda"].ToString();
+                        Session["Desc_Num_UN"] = "";
+
+                        System.Data.DataSet ds = DALServicesM.GetUN();
+
+                        foreach (System.Data.DataRow r in ds.Tables[0].Rows)
+                        {
+                            if (Session["loginTienda"].ToString().Trim().Equals(r[0].ToString().Trim()))
+                            {
+                                Session["Desc_Num_UN"] = r[1].ToString();
+                            }
+                        }
+
+                        return RedirectToAction("OrdenSeleccionada", "Ordenes");
+                        //return RedirectToAction("Index", "Ordenes");
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "CPanel");
+                    }
+
+                    //Session["userFail"] = "Usuario o Password incorrecto";
+                    //return RedirectToAction("Login", "Security");
                 }
             }
 
@@ -57,6 +83,7 @@ namespace ServicesManagement.Web.Controllers
             //return RedirectToAction("Index", "Ordenes");
             //  return View();
         }
+
 
     }
 }
