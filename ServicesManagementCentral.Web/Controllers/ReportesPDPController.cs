491b3d27b76d4d2add9e72a567807ef55d59a98b
Pedro Castillo <agonzalez@itechdev.com.mx>
2021-05-13 12:53:47 -0500

    Reportes PDP

---
+++ ServicesManagementCentral.Web/Controllers/ReportesPDPController.cs
@@ -0,0 +1,37 @@
+﻿using ServicesManagement.Web.Models.ReportesPDP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServicesManagement.Web.Controllers
+{
+    public class ReportesPDPController : Controller
+    {
+        // GET: ReportesPDP
+        #region FormaPago
+
+       
+        public ActionResult FormaPago()
+        {
+            return View();
+        }
+
+        public ActionResult GetFormaPago()
+        {
+            try
+            {
+                var list = new List<ReportesPDP>(); //DataTableToModel.ConvertTo<SuppliersWHForAproval>(DALAlmacenes.SuppliersWHForAproval_sUP().Tables[0]);
+                var result = new { Success = true, resp = list };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception x)
+            {
+                var result = new { Success = false, Message = x.Message };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+        }
+        #endregion
+    }
+}
