b4a5752ee55ee7c3e9312ab99b424331dda43368
Pedro Castillo <agonzalez@itechdev.com.mx>
2021-05-13 12:29:16 -0500

    Modelo Reportes PDP

---
+++ ServicesManagementCentral.Web/Models/ReportesPDP/ReportesPDP.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServicesManagement.Web.Models.ReportesPDP
+{
+    public class ReportesPDP
+    {
+        public string NoOrden { get; set; }
+        public string Fecha { get; set; }
+        public string NoConsignacion { get; set; }
+        public string Monto { get; set; }
+        public string TipoPAgo { get; set; }
+        public string Origen { get; set; }
+        public string FecEntrega { get; set; }
+ 
+    }
+}
