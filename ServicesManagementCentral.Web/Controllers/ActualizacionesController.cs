1ae67e241a2086ad42ee4c217de33730438d1994
Jose Reyes <jreyes@itechdev.com.mx>
2021-05-14 16:08:03 -0500

    Modificación de alertas.

--- ServicesManagementCentral.Web/Controllers/ActualizacionesController.cs
+++ ServicesManagementCentral.Web/Controllers/ActualizacionesController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ServicesManagement.Web.DAL.Actualizaciones;
+using ServicesManagement.Web.Helpers;
 
 namespace ServicesManagement.Web.Controllers
 {
@@ -19,8 +21,21 @@ namespace ServicesManagement.Web.Controllers
         {
             try
             {
-                var archivoTmp = ArchivoCSV;
-                var result = new { Success = true, resp = archivoTmp };
+                String readString = ArchivoCSV;
+                string[] fileExcel = readString.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+                int elemFile = fileExcel.Length;
+                var insEnc = DALActualizaciones.SuppliersWHStockHeader_iUP(int.Parse(fileExcel[1]), int.Parse(fileExcel[2]), 
+                    int.Parse(fileExcel[fileExcel.Length - 1]), int.Parse(fileExcel[fileExcel.Length - 2]), fileExcel[3], fileExcel[4]);
+                var delInv = DALActualizaciones.SuppliersWHStockDetail_dUP(int.Parse(fileExcel[1]), int.Parse(fileExcel[2]));
+
+                for (int i = 7; i + 9 <= fileExcel.Length; i = i + 5)
+                {
+                    var insInv = DALActualizaciones.SuppliersWHStockDetail_iUP(int.Parse(fileExcel[1]), int.Parse(fileExcel[2]), 
+                        decimal.Parse(fileExcel[i-1]), int.Parse(fileExcel[i]), int.Parse(fileExcel[i + 2]), 0, int.Parse(fileExcel[i + 1]), 
+                        fileExcel[3], fileExcel[4]);
+                }
+
+                var result = new { Success = true, resp = readString };
                 return Json(result, JsonRequestBehavior.AllowGet);
             }
             catch (Exception x)
