b47d1c05c39987c78374876cb92424920299950d
Jose Reyes <jreyes@itechdev.com.mx>
2021-05-14 09:49:20 -0500

    Lectura CSV en el controller

--- ServicesManagementCentral.Web/Controllers/ActualizacionesController.cs
+++ ServicesManagementCentral.Web/Controllers/ActualizacionesController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Soriana.FWK;
+using System;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,5 +15,20 @@ namespace ServicesManagement.Web.Controllers
         {
             return View();
         }
+        public ActionResult InsertaArchivo(string ArchivoCSV)
+        {
+            try
+            {
+                var archivoTmp = ArchivoCSV;
+                var result = new { Success = true, resp = archivoTmp };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception x)
+            {
+                var result = new { Success = false, Message = x.Message };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+        }
     }
+
 }
