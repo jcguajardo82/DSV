baed22e94b47f84d13867126fe0271eb24529d6a
Unknown <unknown@example.com>
2021-05-14 18:04:21 -0500

    CargaV02

---
+++ ServicesManagementCentral.Web/Models/SubirArchivos/CargaManuales.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ServicesManagementCentral.Web.Models.SubirArchivos
+{
+    public class CargaManuales
+    {
+        [Required]
+        [DisplayName("Examinar")]public HttpPostedFileBase Examinar { get; set; }
+
+  
+        [DisplayName("Cadena")]
+        public string Cadena { get; set; }
+
+        public int idOwner { get; set; }
+        public string ownerName { get; set; }
+    }
+}
