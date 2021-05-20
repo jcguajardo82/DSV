1135583073df6a47c3651af96a87683ab7fd3d7d
Pedro Castillo <agonzalez@itechdev.com.mx>
2021-05-20 11:35:57 -0500

    Modelos para tipo de envió y Operaciones

---
+++ ServicesManagementCentral.Web/Models/Almacenes/SuppliersWHOperations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServicesManagement.Web.Models.Almacenes
+{
+    public class SuppliersWHOperations
+    {
+        public int idOperType { get; set; }
+        public string OperTypeDesc { get; set; }
+    }
+}
---
+++ ServicesManagementCentral.Web/Models/Almacenes/SuppliersWHShipments.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServicesManagement.Web.Models.Almacenes
+{
+    public class SuppliersWHShipments
+    {
+        public int idShipType { get; set; }
+        public string ShipTypeDesc { get; set; }
+    }
+}
