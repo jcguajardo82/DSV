d19a41c7dec8db2929abd853bbda3192346a1189
Jose Reyes <jreyes@itechdev.com.mx>
2021-05-28 12:16:42 -0500

    Inserción a Datos Maestros.

--- ServicesManagementCentral.Web/DAL/Actualizaciones/DALActualizaciones.cs
+++ ServicesManagementCentral.Web/DAL/Actualizaciones/DALActualizaciones.cs
@@ -140,5 +140,91 @@ namespace ServicesManagement.Web.DAL.Actualizaciones
 
         }
 
+        #region "Datos Maestros"
+        public static DataSet up_Corp_Vendor_Del_Inventario(int idSupplierWH, int idSupplierWHCode)
+        {
+
+            DataSet ds = new DataSet();
+
+            string conection = ConfigurationManager.AppSettings[ConfigurationManager.AppSettings["AmbienteSC"]];
+            if (System.Configuration.ConfigurationManager.AppSettings["flagConectionDBEcriptado"].ToString().Trim().Equals("1"))
+            {
+                conection = Soriana.FWK.FmkTools.Seguridad.Desencriptar(ConfigurationManager.AppSettings[ConfigurationManager.AppSettings["AmbienteSC"]]);
+            }
+
+
+            try
+            {
+                Soriana.FWK.FmkTools.SqlHelper.connection_Name(ConfigurationManager.ConnectionStrings["Connection_DM"].ConnectionString);
+
+                System.Collections.Hashtable parametros = new System.Collections.Hashtable();
+                parametros.Add("@NumProv_LIFNR", idSupplierWH);
+                parametros.Add("@Clave_Almacen", idSupplierWHCode);
+
+                ds = Soriana.FWK.FmkTools.SqlHelper.ExecuteDataSet(CommandType.StoredProcedure, "[dbo].[up_Corp_Vendor_Del_Inventario]", false, parametros);
+
+                return ds;
+            }
+            catch (SqlException ex)
+            {
+
+                throw ex;
+            }
+            catch (System.Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+        public static DataSet up_Corp_Vendor_Ins_Inventario(int idSupplierWH, int idSupplierWHCode, decimal barCode, decimal stockLevel,
+            decimal qtyStockSafety, decimal qtyStockForSale, decimal qtyStockReserved, string StockDate, string StockTime)
+        {
+
+            DataSet ds = new DataSet();
+
+            string conection = ConfigurationManager.AppSettings[ConfigurationManager.AppSettings["AmbienteSC"]];
+            if (System.Configuration.ConfigurationManager.AppSettings["flagConectionDBEcriptado"].ToString().Trim().Equals("1"))
+            {
+                conection = Soriana.FWK.FmkTools.Seguridad.Desencriptar(ConfigurationManager.AppSettings[ConfigurationManager.AppSettings["AmbienteSC"]]);
+            }
+
+
+            try
+            {
+                Soriana.FWK.FmkTools.SqlHelper.connection_Name(ConfigurationManager.ConnectionStrings["Connection_DM"].ConnectionString);
+
+                string[] fecha = StockDate.Split(new string[] { "/" }, StringSplitOptions.RemoveEmptyEntries);
+                string fechaParam = fecha[2] + "/" + fecha[1] + "/" + fecha[0] + " " + StockTime;
+
+                System.Collections.Hashtable parametros = new System.Collections.Hashtable();
+                parametros.Add("@NumProv_LIFNR", idSupplierWH);
+                parametros.Add("@Clave_Almacen", idSupplierWHCode);
+                parametros.Add("@barCode", barCode);
+                parametros.Add("@LABST", stockLevel);
+                parametros.Add("@PreOrden", qtyStockReserved);
+                parametros.Add("@Stock_Seguridad", qtyStockSafety);
+                parametros.Add("@Fec_Movto", fechaParam);
+
+                ds = Soriana.FWK.FmkTools.SqlHelper.ExecuteDataSet(CommandType.StoredProcedure, "[dbo].[up_Corp_Vendor_Ins_Inventario]", false, parametros);
+
+                return ds;
+            }
+            catch (SqlException ex)
+            {
+
+                throw ex;
+            }
+            catch (System.Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+        #endregion
+
     }
 }
