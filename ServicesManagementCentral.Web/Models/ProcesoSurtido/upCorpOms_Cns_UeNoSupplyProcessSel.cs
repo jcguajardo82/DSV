360ca6b52ababa1f727aa749b9d9619951a26a48
Pedro Castillo <agonzalez@itechdev.com.mx>
2021-06-04 14:18:53 -0500

    Auto stash before rebase of "origin/master"

---
+++ ServicesManagementCentral.Web/Models/ProcesoSurtido/upCorpOms_Cns_UeNoSupplyProcessSel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServicesManagement.Web.Models.ProcesoSurtido
+{
+    public class upCorpOms_Cns_UeNoSupplyProcessSel
+    {
+        public int OrderNo { get; set; }
+        public string UeNo { get; set; }
+        public string SKU { get; set; }
+        public string EAN { get; set; }
+        public string Descripcion { get; set; }
+        public double Piezas { get; set; }
+        public int ConGuia { get; set; }
+        public bool Suplido { get; set; }
+
+    }
+}
