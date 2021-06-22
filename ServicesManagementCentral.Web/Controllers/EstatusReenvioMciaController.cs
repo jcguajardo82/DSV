71a4f1dbc602f0d6fe2fbf35099de232724d3c43
Nieves <nhernandez@itechdev.com.mx>
2021-06-22 18:14:34 -0500

    Reenvios Pendientes/Concluidos y Consulta datoss Proveedor/contactos

--- ServicesManagementCentral.Web/Controllers/EstatusReenvioMciaController.cs
+++ ServicesManagementCentral.Web/Controllers/EstatusReenvioMciaController.cs
@@ -42,7 +42,7 @@ namespace ServicesManagement.Web.Controllers
 
         #endregion
 
-        #region EstatusReenvioMcia DSV
+        #region EstatusReenvioMcia DSV Pendientes
 
 
         //GET: SolicitudGuiasReenvio
@@ -73,6 +73,60 @@ namespace ServicesManagement.Web.Controllers
             {
                 List<DetalleEstatusReenvioMcia> list = new List<DetalleEstatusReenvioMcia>();
                 var ds = DALReenvioMciaProveedores.upCorpOMS_Cns_UeNoReshipmentDetail(consignacion);
+
+                if (ds.Tables.Count > 0)
+                    list = DataTableToModel.ConvertTo<DetalleEstatusReenvioMcia>(ds.Tables[0]);
+
+                var result = new { Success = true, resp = list };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception x)
+            {
+                var result = new { Success = false, Message = x.Message };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        #endregion
+
+        #region EstatusReenvioMcia DSV Concluidos
+
+
+        //GET: SolicitudGuiasReenvio
+
+
+        //GET: RenviodeMercancia
+
+
+        public ActionResult EstatusReenvioMciaConcluidos()
+        {
+            return View();
+
+        }
+
+        public ActionResult GetreenviomciaDSVConcluidos()
+        {
+
+            try
+            {
+                var list = DataTableToModel.ConvertTo<ReenvioMciaProveedor>(DALReenvioMciaProveedores.upCorpOMS_Cns_UeNoReShipmentConcluidos().Tables[0]);
+                var result = new { Success = true, resp = list };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception x)
+            {
+                var result = new { Success = false, Message = x.Message };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+        public ActionResult GetDetalleConsignacionDSVConcluidos(string consignacion)
+        {
+            try
+            {
+                List<DetalleEstatusReenvioMcia> list = new List<DetalleEstatusReenvioMcia>();
+                var ds = DALReenvioMciaProveedores.upCorpOMS_Cns_UeNoReshipmentDetailconcluidos(consignacion);
 
                 if (ds.Tables.Count > 0)
                     list = DataTableToModel.ConvertTo<DetalleEstatusReenvioMcia>(ds.Tables[0]);
