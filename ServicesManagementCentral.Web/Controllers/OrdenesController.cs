09f76437d6143b62d9fa82f6f8c68e44bf2d9706
eliseo gijon romero <egijon@itechdev.com.mx>
2021-06-23 02:31:15 -0500

    Controller Orden, ajustes para manejo de UE

--- ServicesManagementCentral.Web/Controllers/OrdenesController.cs
+++ ServicesManagementCentral.Web/Controllers/OrdenesController.cs
@@ -484,9 +484,7 @@ namespace ServicesManagement.Web.Controllers
 
                         o.Orden = new InformacionDetalleOrden();
 
-
-                        o.Orden.NumeroOrden = OrderNo;
-
+                        o.Orden.NumeroOrden = OrderNo.Replace("-","");
 
                         o.Orden.EstatusUnidadEjecucion = status;
                         o.Orden.NumeroUnidadEjecucion = ue;
@@ -513,7 +511,6 @@ namespace ServicesManagement.Web.Controllers
                         System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
                         Soriana.FWK.FmkTools.LoggerToFile.WriteToLogFile(Soriana.FWK.FmkTools.LogModes.LogError, Soriana.FWK.FmkTools.LogLevel.INFO, "in_data: " + json2, false, null);
-
 
                         Soriana.FWK.FmkTools.LoggerToFile.WriteToLogFile(Soriana.FWK.FmkTools.LogModes.LogError, Soriana.FWK.FmkTools.LogLevel.INFO, "Request: " + apiUrl, false, null);
 
@@ -557,9 +554,7 @@ namespace ServicesManagement.Web.Controllers
 
                         o.Orden = new InformacionDetalleOrden();
 
-
-                        o.Orden.NumeroOrden = array[0];
-
+                        o.Orden.NumeroOrden = array[0].Replace("-","");
 
                         o.Orden.EstatusUnidadEjecucion = status;
                         o.Orden.NumeroUnidadEjecucion = ue;
@@ -632,7 +627,7 @@ namespace ServicesManagement.Web.Controllers
                 return Json(result, JsonRequestBehavior.AllowGet);
 
             }
-        }
+         }
         [HttpPost]
         public ActionResult FinalizarEntrega(List<FinEmbarque> Guias)
         {
