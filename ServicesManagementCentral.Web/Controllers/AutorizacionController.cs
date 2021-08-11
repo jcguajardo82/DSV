67a52622861b87edd0ef43a59cadb46743fdf8c3
Pedro Castillo <agonzalez@itechdev.com.mx>
2021-08-11 11:19:48 -0500

    Se agrega modulo de Autorizacion RMA

---
+++ ServicesManagementCentral.Web/Controllers/AutorizacionController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServicesManagement.Web.Controllers
+{
+    [Authorize]
+    public class AutorizacionController : Controller
+    {
+        // GET: Autorizacion
+        public ActionResult Autorizacion()
+        {
+            return View();
+        }
+    }
+}
