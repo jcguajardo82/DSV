4fe1e317e49d4c5f21b0d0632e0fe9e29871dbc5
Pedro Castillo <agonzalez@itechdev.com.mx>
2021-08-30 10:46:57 -0500

    Se crean modelos para el requerimiento Tms Embarque

---
+++ ServicesManagementCentral.Web/Models/ShipmentRequests/ShipmentUCC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServicesManagement.Web.Models.ShipmentRequests
+{
+    public class ShipmentUCC
+    {
+        public string IdShipmentWMS { get; set; }
+        public string UeNo { get; set; }
+        public string OrderNo { get; set; }
+        public string CnscOrder { get; set; }
+        public string Ucc { get; set; }
+        public string CreatedDate { get; set; }
+
+        public string createdUser { get; set; }
+
+    }
+}
