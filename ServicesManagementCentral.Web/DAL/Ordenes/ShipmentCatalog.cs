28fa04eab126a62819256da3861e2a8a682383ce
Jose Reyes <jreyes@itechdev.com.mx>
2021-08-30 10:58:08 -0500

    Nuevo Dal de catálogo.

---
+++ ServicesManagementCentral.Web/DAL/Ordenes/ShipmentCatalog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServicesManagement.Web.DAL.Ordenes
+{
+    public class ShipmentCatalog
+    {
+    }
+}
