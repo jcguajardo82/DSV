8932e440fc26ebe61db5e09ebad35fef7df9c10b
Pedro Castillo <agonzalez@itechdev.com.mx>
2021-10-20 00:56:04 -0500

    Se agrega Articulo Flete

---
+++ ServicesManagementCentral.Web/Models/CallCenter/CostoFlete.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServicesManagement.Web.Models.CallCenter
+{
+    public class CostoFlete
+    {
+        public string CostoEnvio { get; set; }
+        public string Num_CodBarra { get; set; }
+        public string id_Num_Sku { get; set; }
+        public string Desc_art { get; set; }
+        public string Cve_UnVta { get; set; }
+                                                                                                                          
+    }
+}
