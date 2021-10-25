4f558121c8e24bd219222b2c14df458472aef68f
Pedro Castillo <agonzalez@itechdev.com.mx>
2021-10-25 17:50:43 -0500

    Se agrega Historial de Carrito de Compras

---
+++ ServicesManagementCentral.Web/Models/CallCenter/Historial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServicesManagement.Web.Models.CallCenter
+{
+    public class Historial
+    {
+        public int Id_Num_Orden { get; set; }
+        public int OrderNo { get; set; }
+        public string OrderDate { get; set; }
+        public decimal Imp_Pago { get; set; }
+        public string Id_Num_Car { get; set; }
+        public string UeNo { get; set; }
+        public decimal TotalArt { get; set; }
+
+    }
+}
