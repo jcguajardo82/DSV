f63c4ee14909eb19e26e233266f12264bbdf3866
Pedro Castillo <agonzalez@itechdev.com.mx>
2021-10-27 11:12:11 -0500

    Se agrega opción Sustituto en carrito

--- ServicesManagementCentral.Web/Models/CallCenter/ArtCar_d.cs
+++ ServicesManagementCentral.Web/Models/CallCenter/ArtCar_d.cs
@@ -20,12 +20,15 @@ namespace ServicesManagement.Web.Models.CallCenter
         public string obs { get; set; }
         public string Desc_art { get; set; }
         public string Cve_UnVta { get; set; }
+
         public decimal Num_CodBarra { get; set; }
+
+        public string Sustituto { get; set; }
         //public string Impto { get; set; }
         //public string Bit_SkuObligadoComp { get; set; }
         //public string Bit_SkuObligadoSurt { get; set; }
         //public string Fec_Movto { get; set; }
-                                                                 
+
     }
 
 
