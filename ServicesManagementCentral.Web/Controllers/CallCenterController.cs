f61c3de38c533899dd825163296ef682171e89e3
Jose Reyes <jreyes@itechdev.com.mx>
2021-11-03 18:26:03 -0600

    se cierra función

--- ServicesManagementCentral.Web/Controllers/CallCenterController.cs
+++ ServicesManagementCentral.Web/Controllers/CallCenterController.cs
@@ -1834,8 +1834,9 @@ namespace ServicesManagement.Web.Controllers
 
         public bool TieneCobertura(int tienda, string codigopostal)
         {
-
             bool puede = DALCallCenter.upCorpOms_Cns_CoberturaTienda(tienda, codigopostal);
+            return puede;
+        }
 
         public ActionResult GetCategorias()
         {
