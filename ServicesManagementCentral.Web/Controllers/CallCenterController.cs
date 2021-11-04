728d8f15e256b233cb5082be38372c820a99a39d
Jose Reyes <jreyes@itechdev.com.mx>
2021-11-03 18:31:09 -0600

    =  ""

--- ServicesManagementCentral.Web/Controllers/CallCenterController.cs
+++ ServicesManagementCentral.Web/Controllers/CallCenterController.cs
@@ -1876,7 +1876,7 @@ namespace ServicesManagement.Web.Controllers
 
         }
 
-        public ActionResult GetFlete(int StoreNum, string CodigoPostal)
+        public ActionResult GetFlete(int StoreNum, string CodigoPostal = "")
         {
             try
             {
