288b283481818c5c62c4d8af04c1d05403d6559a
Jose Reyes <jreyes@itechdev.com.mx>
2021-11-03 18:37:17 -0600

    Historial DAL

--- ServicesManagementCentral.Web/DAL/CallCenter/DALCallCenter.cs
+++ ServicesManagementCentral.Web/DAL/CallCenter/DALCallCenter.cs
@@ -1613,7 +1613,47 @@ namespace ServicesManagement.Web.DAL.CallCenter
             }
 
         }
-       
+
+
+        //public static DataSet HistorialOrdenes_sUP(int Id_Num_Cte)
+        //{
+
+
+
+        //    DataSet ds = new DataSet();
+
+        //    string conection = ConfigurationManager.AppSettings[ConfigurationManager.AppSettings["AmbienteSC"]];
+        //    if (System.Configuration.ConfigurationManager.AppSettings["flagConectionDBEcriptado"].ToString().Trim().Equals("1"))
+        //    {
+        //        conection = Soriana.FWK.FmkTools.Seguridad.Desencriptar(ConfigurationManager.AppSettings[ConfigurationManager.AppSettings["AmbienteSC"]]);
+        //    }
+
+        //    try
+        //    {
+        //        Soriana.FWK.FmkTools.SqlHelper.connection_Name(ConfigurationManager.ConnectionStrings["Connection_DEV"].ConnectionString);
+        //        System.Collections.Hashtable parametros = new System.Collections.Hashtable();
+
+        //        parametros.Add("@Id_Num_Cte", Id_Num_Cte);
+
+
+
+        //        ds = Soriana.FWK.FmkTools.SqlHelper.ExecuteDataSet(CommandType.StoredProcedure, "carrito.HistorialOrdenes_sUP", false, parametros);
+
+        //        return ds;
+        //    }
+        //    catch (SqlException ex)
+        //    {
+
+        //        throw ex;
+        //    }
+        //    catch (System.Exception ex)
+        //    {
+
+        //        throw ex;
+        //    }
+
+        //}
+
         #endregion
     }
 }
