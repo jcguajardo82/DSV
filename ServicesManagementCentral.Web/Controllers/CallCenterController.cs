51671fb1566e56b9e0e46211a322e102f5b7e438
Pedro Castillo <agonzalez@itechdev.com.mx>
2021-11-10 15:16:15 -0600

    Editar Usuario

--- ServicesManagementCentral.Web/Controllers/CallCenterController.cs
+++ ServicesManagementCentral.Web/Controllers/CallCenterController.cs
@@ -1299,10 +1299,10 @@ namespace ServicesManagement.Web.Controllers
 
                 GetClient list = new GetClient();
 
-                //list = DataTableToModel.ConvertTo<GetClient>(DALCallCenter.GetClientByPhoneEmail(Id_Email).Tables[0]).FirstOrDefault();
+                list = DataTableToModel.ConvertTo<GetClient>(DALCallCenter.GetClientByPhoneEmail(Telefono).Tables[0]).FirstOrDefault();
 
-                //if (list == null && Id_Cnsc_DirCTe == 0 && Id_Num_Cte == 0)
-                if ( Id_Cnsc_DirCTe == 0 && Id_Num_Cte == 0)
+                if (list == null && Id_Cnsc_DirCTe == 0 && Id_Num_Cte == 0)
+                //if ( Id_Cnsc_DirCTe == 0 && Id_Num_Cte == 0)
                 {
 
 
@@ -1310,7 +1310,7 @@ namespace ServicesManagement.Web.Controllers
 
                     DALCallCenter.Email_iUp(Id_Num_Cte, Id_Email, TarjetaLealtad);
                     DALCallCenter.DirCte_iUp(Id_Num_Cte, 1, Ids_Num_Edo, Calle, Nom_DirCTe, Num_Ext, Num_Int, Ciudad, Cod_Postal, Colonia, Telefono);
-                    list = DataTableToModel.ConvertTo<GetClient>(DALCallCenter.GetClientByPhoneEmail(Id_Email).Tables[0]).FirstOrDefault();
+                    list = DataTableToModel.ConvertTo<GetClient>(DALCallCenter.GetClientByPhoneEmail(Telefono).Tables[0]).FirstOrDefault();
 
 
 
@@ -1324,7 +1324,7 @@ namespace ServicesManagement.Web.Controllers
                         DALCallCenter.Cte_uUp(Id_Num_Cte, Nom_Cte, Ap_Paterno, Ap_Materno);
                         DALCallCenter.Email_uUp(Id_Num_Cte, Id_Email, TarjetaLealtad);
                         DALCallCenter.DirCte_uUp(Id_Cnsc_DirCTe, Id_Num_Cte, 1, Ids_Num_Edo, Calle, Nom_DirCTe, Num_Ext, Num_Int, Ciudad, Cod_Postal, Colonia, Telefono);
-                        list = DataTableToModel.ConvertTo<GetClient>(DALCallCenter.GetClientByPhoneEmail(Id_Email).Tables[0])
+                        list = DataTableToModel.ConvertTo<GetClient>(DALCallCenter.GetClientByPhoneEmail(Telefono).Tables[0])
                             .Where(x => x.Id_Cnsc_DirCTe == Id_Cnsc_DirCTe.ToString()).FirstOrDefault();
 
                     }
