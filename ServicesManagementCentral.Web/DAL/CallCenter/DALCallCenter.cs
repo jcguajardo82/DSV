36c35281a000ea3278079da27a736fe3867e1066
Pedro Castillo <agonzalez@itechdev.com.mx>
2021-11-30 13:32:58 -0600

    PaymentsGetCancelacion
    Se agrega al flujo, el llamado al servicio PaymentsGetCancelacion

--- ServicesManagementCentral.Web/DAL/CallCenter/DALCallCenter.cs
+++ ServicesManagementCentral.Web/DAL/CallCenter/DALCallCenter.cs
@@ -1743,6 +1743,43 @@ namespace ServicesManagement.Web.DAL.CallCenter
         }
         #endregion
 
+        #region End Point Cancelacion
+        public static DataSet PaymentsGetCancelacion_sUp(int Id_cancelacion)
+        {
 
+            DataSet ds = new DataSet();
+
+            string conection = ConfigurationManager.AppSettings[ConfigurationManager.AppSettings["AmbienteSC"]];
+            if (System.Configuration.ConfigurationManager.AppSettings["flagConectionDBEcriptado"].ToString().Trim().Equals("1"))
+            {
+                conection = Soriana.FWK.FmkTools.Seguridad.Desencriptar(ConfigurationManager.AppSettings[ConfigurationManager.AppSettings["AmbienteSC"]]);
+            }
+
+            try
+            {
+                Soriana.FWK.FmkTools.SqlHelper.connection_Name(ConfigurationManager.ConnectionStrings["Connection_DEV"].ConnectionString);
+                System.Collections.Hashtable parametros = new System.Collections.Hashtable();
+
+                parametros.Add("@Id_cancelacion", Id_cancelacion);
+              
+
+
+                ds = Soriana.FWK.FmkTools.SqlHelper.ExecuteDataSet(CommandType.StoredProcedure, "[dbo].[PaymentsGetCancelacion_sUp]", false, parametros);
+
+                return ds;
+            }
+            catch (SqlException ex)
+            {
+
+                throw ex;
+            }
+            catch (System.Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+        #endregion
     }
 }
