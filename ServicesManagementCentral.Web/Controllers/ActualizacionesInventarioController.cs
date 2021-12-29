75a6db28c5973b0afaf34737bde645e810036cb7
Alex Jonguitud <ajonguituddelangel@suppliers.tenaris.com>
2021-12-29 14:36:10 -0600

    Ajustes en Plantilla de Inventario por Proveedor

--- ServicesManagementCentral.Web/Controllers/ActualizacionesInventarioController.cs
+++ ServicesManagementCentral.Web/Controllers/ActualizacionesInventarioController.cs
@@ -8,6 +8,7 @@ using ServicesManagementCentral.Web.Controllers.Api;
 using ServicesManagement.Web.Models;
 using ServicesManagement.Web.DAL.ActualizacionesInventario;
 using ServicesManagement.Web.Helpers;
+using System.IO;
 
 namespace ServicesManagement.Web.Controllers
 {
@@ -17,6 +18,24 @@ namespace ServicesManagement.Web.Controllers
         // GET: SolicitudGuiasReenvio
         public ActionResult ActualizacionesInventario()
         {
+            // EXPORTACION A EXCEL
+            string excel = CrearExcel();
+            MemoryStream stream = null;
+
+            stream = ExcelTools.LoadAndConvertToMemStream(excel);
+            Response.Clear();
+            Response.Buffer = true;
+            Response.Charset = "";
+            Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            Response.AddHeader("content-disposition", "attachment;filename= Reporte.xlsm");
+
+            stream.WriteTo(Response.OutputStream);
+            Response.Flush();
+            Response.End();
+
+
+            if (System.IO.File.Exists(excel))
+                System.IO.File.Delete(excel);
             return View();
         }
 
@@ -28,6 +47,94 @@ namespace ServicesManagement.Web.Controllers
         public ActionResult NotasdeCredito()
         {
             return View();
+        }
+
+        public ActionResult ExportToExcelCVPTL(string WorkOrder)
+        {
+            string Cv = string.Empty;
+            string Partida = string.Empty;
+
+            string GetCVP = string.Empty;
+
+            GetCVP = WorkOrder;
+            Cv = GetCVP.Split('/')[0];
+            Partida = GetCVP.Split('/')[1];
+
+            // EXPORTACION A EXCEL
+            string excel = CrearExcel();
+            MemoryStream stream = null;
+
+            stream = ExcelTools.LoadAndConvertToMemStream(excel);
+            Response.Clear();
+            Response.Buffer = true;
+            Response.Charset = "";
+            Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            Response.AddHeader("content-disposition", "attachment;filename= Reporte.xlsx");
+
+            stream.WriteTo(Response.OutputStream);
+            Response.Flush();
+            Response.End();
+
+
+            if (System.IO.File.Exists(excel))
+                System.IO.File.Delete(excel);
+            return View();
+        }
+
+        public string CrearExcel()
+        {
+
+            OfficeOpenXml.ExcelWorksheet xlsWorkSheet = null;
+            //     OfficeOpenXml.ExcelWorksheet xlsWorkSheetSumary = null;
+            OfficeOpenXml.ExcelWorkbook workBook = null;
+            OfficeOpenXml.ExcelPackage xlsApp = null;
+            string fileSavedPath = "fileName";
+            bool mismaFila = false;
+            int fila = 1, vuelta = 0, piezas = 0, filaSumas = 0;
+            decimal metros = 0, ton = 0;
+
+            string templatePath = Server.MapPath("~/Files/");
+
+            // OSR SRI.356915 {
+            fileSavedPath = ExcelTools.OpenExcelTemplate(ref workBook, ref xlsApp, templatePath, "DSV Macro v1.42");
+            ExcelTools.SelectWorkSheet(ref workBook, ref xlsWorkSheet, "Datos");
+            // ExcelTools.SelectWorkSheet(ref workBook, ref xlsWorkSheetSumary, "Resumen");
+
+            xlsWorkSheet.Name = "Datos";
+
+
+
+                                    ExcelTools.InsertCell2(ref xlsApp, ref xlsWorkSheet, fila, 1, "Chocolate", false, 11, "Frutiger 45 Light", true,
+                                               System.Drawing.Color.White, System.Drawing.Color.Black, true, null, OfficeOpenXml.Style.ExcelBorderStyle.Dashed,
+                                               OfficeOpenXml.Style.ExcelHorizontalAlignment.Center);
+
+                                    ExcelTools.InsertCell2(ref xlsApp, ref xlsWorkSheet, fila, 2, "Chocolate", false, 11, "Frutiger 45 Light", true,
+                                                                   System.Drawing.Color.White, System.Drawing.Color.Black, true, null, OfficeOpenXml.Style.ExcelBorderStyle.Dashed,
+                                                                   OfficeOpenXml.Style.ExcelHorizontalAlignment.Center);
+
+                                    ExcelTools.InsertCell2(ref xlsApp, ref xlsWorkSheet, fila, 3, "Chocolate", false, 11, "Frutiger 45 Light", true,
+                                                                   System.Drawing.Color.White, System.Drawing.Color.Black, true, null, OfficeOpenXml.Style.ExcelBorderStyle.Dashed,
+                                                                   OfficeOpenXml.Style.ExcelHorizontalAlignment.Center);
+
+                                    ExcelTools.InsertCell2(ref xlsApp, ref xlsWorkSheet, fila, 4, "Chocolate", false, 11, "Frutiger 45 Light", true,
+                                                                   System.Drawing.Color.White, System.Drawing.Color.Black, true, null, OfficeOpenXml.Style.ExcelBorderStyle.Dashed,
+                                                                   OfficeOpenXml.Style.ExcelHorizontalAlignment.Center);
+
+                                    ExcelTools.InsertCell2(ref xlsApp, ref xlsWorkSheet, fila, 5, "Chocolate", false, 11, "Frutiger 45 Light", true,
+                                                                   System.Drawing.Color.White, System.Drawing.Color.Black, true, null, OfficeOpenXml.Style.ExcelBorderStyle.Dashed,
+                                                                   OfficeOpenXml.Style.ExcelHorizontalAlignment.Center);
+
+                                    ExcelTools.InsertCell2(ref xlsApp, ref xlsWorkSheet, fila, 6, "Chocolate", false, 11, "Frutiger 45 Light", true,
+                                                                   System.Drawing.Color.White, System.Drawing.Color.Black, true, null, OfficeOpenXml.Style.ExcelBorderStyle.Dashed,
+                                                                   OfficeOpenXml.Style.ExcelHorizontalAlignment.Center);
+
+            if (xlsApp != null && xlsWorkSheet != null)
+            {
+                //string fileName = templateName + "_" + new Random().Next(0, 100000);
+                ExcelTools.SaveFile(ref workBook, ref xlsApp);
+            }
+
+            return fileSavedPath;
         }
     }
 }
