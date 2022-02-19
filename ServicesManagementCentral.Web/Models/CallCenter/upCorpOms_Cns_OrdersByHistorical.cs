0cc4f532c41b990899167b6c6a6bfacac9d221e3
eliseo gijon romero <egijon@itechdev.com.mx>
2022-02-18 23:09:19 -0600

    Modelo Callcenter, Controlador Ordenes para Envia.com

--- ServicesManagementCentral.Web/Models/CallCenter/upCorpOms_Cns_OrdersByHistorical.cs
+++ ServicesManagementCentral.Web/Models/CallCenter/upCorpOms_Cns_OrdersByHistorical.cs
@@ -9,7 +9,6 @@ namespace ServicesManagement.Web.Models.CallCenter
     {
         public string OrderNo { get; set; }
         public string UeNo { get; set; }
-        public string StatusDescriptionUE { get; set; }
         public string PosBarcode { get; set; }
         public string ProductId { get; set; }
         public string ProductName { get; set; }
@@ -18,7 +17,10 @@ namespace ServicesManagement.Web.Models.CallCenter
         public decimal Price { get; set; }
         public string ArticuloFoto { get; set; }
 
-
+        public int StatusUe { get; set; }
+        public string StatusDescriptionUE { get; set; }
+        public string StatusDescription { get; set; }
+        public byte StatusId { get; set; }
 
     }
 }
