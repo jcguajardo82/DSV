c35a5058c7ab70eccad854c863949658625aefb7
Alex Jonguitud <ajonguituddelangel@suppliers.tenaris.com>
2022-02-18 20:12:41 -0600

    Se Agrega "Descarga de Ordenes Almacenes" V1

---
+++ ServicesManagementCentral.Web/Controllers/DescargaOrdenesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServicesManagement.Web.Controllers
+{
+    public class DescargaOrdenesController : Controller
+    {
+        // GET: DescargaOrdenes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
