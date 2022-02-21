ccb671fd0803b523bcca958b4ea367d5453f7a85
Jose Reyes <jreyes@itechdev.com.mx>
2022-02-21 10:50:04 -0600

    Validación Urgente RMA v7.1 (cherry pick)

--- ServicesManagementCentral.Web/Models/CallCenter/upCorpOms_Cns_OrdersByHistorical.cs
+++ ServicesManagementCentral.Web/Models/CallCenter/upCorpOms_Cns_OrdersByHistorical.cs
@@ -9,7 +9,6 @@ namespace ServicesManagement.Web.Models.CallCenter
     {
         public string OrderNo { get; set; }
         public string UeNo { get; set; }
-        public string StatusDescriptionUE { get; set; }
         public string PosBarcode { get; set; }
         public string ProductId { get; set; }
         public string ProductName { get; set; }
@@ -17,8 +16,6 @@ namespace ServicesManagement.Web.Models.CallCenter
         public decimal SubTotal { get; set; }
         public decimal Price { get; set; }
         public string ArticuloFoto { get; set; }
-
-
         public int StatusUe { get; set; }
         public string StatusDescriptionUE { get; set; }
         public string StatusDescription { get; set; }
