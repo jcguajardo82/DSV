db073aa1fe8a2eaa218a1fec3d1bbc846e0ac718
Jose Reyes <jreyes@itechdev.com.mx>
2022-03-03 13:24:20 -0600

    Monitor Ordenes 5
    Se modificó el título. Se mapearon los campos: guía, comentarios y recibo en el modal.

--- ServicesManagementCentral.Web/Controllers/ProcesoReciboDevolucionesController.cs
+++ ServicesManagementCentral.Web/Controllers/ProcesoReciboDevolucionesController.cs
@@ -162,7 +162,14 @@ namespace ServicesManagement.Web.Controllers
                         rol.idRol = item["rol"].ToString();
                         rol.nombreRol = item["nombreRol"].ToString();
                         Session["UserRol"] = rol;
-                        idOwner = 4;
+                        if (item["idOwner"].ToString() != "")
+                        {
+                            idOwner = int.Parse(item["idOwner"].ToString());
+                        }
+                        else
+                        {
+                            idOwner = 4;
+                        }
                     }
                 }
 
@@ -199,9 +206,17 @@ namespace ServicesManagement.Web.Controllers
                         rol.idRol = item["rol"].ToString();
                         rol.nombreRol = item["nombreRol"].ToString();
                         Session["UserRol"] = rol;
-                        idOwner = 4;
+                        if (item["idOwner"].ToString() != "")
+                        {
+                            idOwner = int.Parse(item["idOwner"].ToString());
+                        }
+                        else
+                        {
+                            idOwner = 4;
+                        }
                     }
                 }
+
                 var list = DataTableToModel.ConvertTo<upCorpOms_Cns_UeDevolCauses>(DALProcesoReciboDevoluciones.upCorpOms_Cns_UeDevolCauses(idOwner).Tables[0]);
 
                 var result = new { Success = true, resp = list };
@@ -235,7 +250,14 @@ namespace ServicesManagement.Web.Controllers
                         rol.idRol = item["rol"].ToString();
                         rol.nombreRol = item["nombreRol"].ToString();
                         Session["UserRol"] = rol;
-                        idOwner = 3;
+                        if (item["idOwner"].ToString() != "")
+                        {
+                            idOwner = int.Parse(item["idOwner"].ToString());
+                        }
+                        else
+                        {
+                            idOwner = 4;
+                        }
                     }
                 }
 
@@ -273,7 +295,14 @@ namespace ServicesManagement.Web.Controllers
                         rol.idRol = item["rol"].ToString();
                         rol.nombreRol = item["nombreRol"].ToString();
                         Session["UserRol"] = rol;
-                        idOwner = 3;
+                        if (item["idOwner"].ToString() != "")
+                        {
+                            idOwner = int.Parse(item["idOwner"].ToString());
+                        }
+                        else
+                        {
+                            idOwner = 4;
+                        }
                     }
                 }
 
