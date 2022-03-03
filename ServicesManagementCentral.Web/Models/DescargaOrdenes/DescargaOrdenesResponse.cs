9337585a1e95f0ade64c053092476b6d0e0aa68e
Alex Jonguitud <ajungitud@itechdev.com.mx>
2022-03-02 18:26:43 -0600

    Se conecta al enpoint la Dercarga de Ordenes

---
+++ ServicesManagementCentral.Web/Models/DescargaOrdenes/DescargaOrdenesResponse.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServicesManagement.Web.Models.DescargaOrdenes
+{
+    public class DescargaOrdenesResponse
+    {
+        public int statusCode { get; set; }
+        public string statusDescription { get; set; }
+        public string url { get; set; }
+    }
+}
