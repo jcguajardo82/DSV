9129576d3bd5bbe59857ac090af413893c500261
Alex Jonguitud <ajungitud@itechdev.com.mx>
2022-03-14 18:10:42 -0600

    Priorizacion de Inventario

---
+++ ServicesManagementCentral.Web/Controllers/PrioridadInventarioController.cs
@@ -0,0 +1,57 @@
+﻿using ServicesManagement.Web.DAL.PriorizacionInventario;
+using ServicesManagement.Web.Helpers;
+using ServicesManagement.Web.Models.PriorizacionInventario;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServicesManagement.Web.Controllers
+{
+    public class PrioridadInventarioController : Controller
+    {
+        // GET: PrioridadInventario
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult GetPrioridadInventario()
+        {
+            try
+            {
+                DataSet ds = DALPriorizacionInventario.PrioridadInventario();
+                List<PriorizacionInventarioModel> listC = DataTableToModel.ConvertTo<PriorizacionInventarioModel>(ds.Tables[0]);
+                var result = new { Success = true, json = listC };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                var result = new { Success = false, Message = ex.Message };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [HttpPost]
+        public ActionResult PrioridadInventario(string json)
+        {
+            try
+            {
+                var result= new Object();
+                DataSet ds = DALPriorizacionInventario.PrioridadInventario(json, User.Identity.Name);
+
+                if (ds.Tables[0].Rows[0][0].ToString().Equals("OK"))
+                    result = new { Success = true };
+                else
+                    result = new { Success = false, Message = "Hubo un error al Insertar" };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                var result = new { Success = false, Message = ex.Message };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ ServicesManagementCentral.Web/DAL/PriorizacionInventario/DALPriorizacionInventario.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ServicesManagement.Web.DAL.PriorizacionInventario
+{
+    public class DALPriorizacionInventario
+    {
+        public static DataSet PrioridadInventario()
+        {
+
+            DataSet ds = new DataSet();
+
+            string conection = ConfigurationManager.AppSettings[ConfigurationManager.AppSettings["AmbienteSC"]];
+            if (System.Configuration.ConfigurationManager.AppSettings["flagConectionDBEcriptado"].ToString().Trim().Equals("1"))
+            {
+                conection = Soriana.FWK.FmkTools.Seguridad.Desencriptar(ConfigurationManager.AppSettings[ConfigurationManager.AppSettings["AmbienteSC"]]);
+            }
+
+
+            try
+            {
+                Soriana.FWK.FmkTools.SqlHelper.connection_Name(ConfigurationManager.ConnectionStrings["Connection_DEV"].ConnectionString);
+
+                System.Collections.Hashtable parametros = new System.Collections.Hashtable();
+
+                ds = Soriana.FWK.FmkTools.SqlHelper.ExecuteDataSet(CommandType.StoredProcedure, "[dbo].[upCorpOms_Cns_PrioridadInventario]", false, parametros);
+
+                return ds;
+            }
+            catch (SqlException ex)
+            {
+
+                throw ex;
+            }
+            catch (System.Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+        public static DataSet PrioridadInventario(string json, string createdUser)
+        {
+
+            DataSet ds = new DataSet();
+
+            string conection = ConfigurationManager.AppSettings[ConfigurationManager.AppSettings["AmbienteSC"]];
+            if (System.Configuration.ConfigurationManager.AppSettings["flagConectionDBEcriptado"].ToString().Trim().Equals("1"))
+            {
+                conection = Soriana.FWK.FmkTools.Seguridad.Desencriptar(ConfigurationManager.AppSettings[ConfigurationManager.AppSettings["AmbienteSC"]]);
+            }
+
+
+            try
+            {
+                Soriana.FWK.FmkTools.SqlHelper.connection_Name(ConfigurationManager.ConnectionStrings["Connection_DEV"].ConnectionString);
+
+                System.Collections.Hashtable parametros = new System.Collections.Hashtable();
+
+                parametros.Add("@json", json);
+                parametros.Add("@createdUser", createdUser);
+
+                ds = Soriana.FWK.FmkTools.SqlHelper.ExecuteDataSet(CommandType.StoredProcedure, "[dbo].[upCorpOms_Upd_PrioridadInventario]", false, parametros);
+
+                return ds;
+            }
+            catch (SqlException ex)
+            {
+
+                throw ex;
+            }
+            catch (System.Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+    }
+}
